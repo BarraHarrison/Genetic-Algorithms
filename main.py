7885656a3504598b8b49fe4ef30e9eed623a4ad7
BarraHarrison <143306095+BarraHarrison@users.noreply.github.com>
2025-03-18 21:31:48 +0900

    Trying to fill up the positions (Genome Length)

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # Genetic Algorithms in Python
+# Used to solve optimization problems
+
+# No packages need to be installed for the example
+
+import random
+
+POPULATION_SIZE = 100
+GENOME_LENGTH = 20
+MUTATION_RATE = 0.01
+CROSSOVER_RATE = 0.70
+GENERATIONS = 200
+
+def random_genome():
+    pass
